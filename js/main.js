75b94ef7dd3a6f8b9dc8f0562f45506e19807b57
nigi4 <luzanikita@gmail.com>
2018-11-24 16:33:30 +0200

    Option button added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-click()
+click();
+// page1();
+page2();
 
 function click() {
     document.getElementById('profile').onclick = function(){
         document.getElementsByClassName('selected')[0].classList.remove('selected');
         this.classList.add('selected');
+        document.getElementById('page2').style.display = 'none';
+        document.getElementById('page1').style.display = 'block';
     }
 
     document.getElementById('diet').onclick = function(){
         document.getElementsByClassName('selected')[0].classList.remove('selected');
         this.classList.add('selected');
+        document.getElementById('page2').style.display = 'block';
+        document.getElementById('page1').style.display = 'none';
+    }
+}
+
+function page2() {
+    var options = false;
+    document.getElementById('add').onclick = function() {
+        if (!options) {
+            document.getElementById('plus').style.transform = 'rotate(45deg)';
+            document.getElementById('food').style.display = 'block';
+            document.getElementById('sport').style.display = 'block';
+        }
+        else {
+            document.getElementById('plus').style.transform = 'rotate(0deg)';
+            document.getElementById('food').style.display = 'none';
+            document.getElementById('sport').style.display = 'none';
+        }
+        options = !options;
     }
 }
