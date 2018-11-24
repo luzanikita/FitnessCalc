bdcc1de03689c1c5075e216f487e13a70018c282
nigi4 <luzanikita@gmail.com>
2018-11-24 16:55:15 +0200

    Option labels added

--- js/main.js
+++ js/main.js
@@ -22,14 +22,18 @@ function page2() {
     var options = false;
     document.getElementById('add').onclick = function() {
         if (!options) {
-            document.getElementById('plus').style.transform = 'rotate(45deg)';
+            document.getElementById('plus').style.transform = 'rotate(135deg)';
             document.getElementById('food').style.display = 'block';
             document.getElementById('sport').style.display = 'block';
+            document.getElementById('food_label').style.display = 'block';
+            document.getElementById('sport_label').style.display = 'block';
         }
         else {
             document.getElementById('plus').style.transform = 'rotate(0deg)';
             document.getElementById('food').style.display = 'none';
             document.getElementById('sport').style.display = 'none';
+            document.getElementById('food_label').style.display = 'none';
+            document.getElementById('sport_label').style.display = 'none';
         }
         options = !options;
     }
